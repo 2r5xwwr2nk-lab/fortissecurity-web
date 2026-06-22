2810d420263456eb7cb826643fd8016818a2b240
2r5xwwr2nk-lab <timeastehlikova@gmail.com>
2026-06-22 16:41:59 +0200

    add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://fortissecurity.eu",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fortissecurity.eu/sluzby",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fortissecurity.eu/kontakt",
+      lastModified: new Date(),
+    },
+  ];
+}
